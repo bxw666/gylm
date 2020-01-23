bd4ae0ce0333f1ea85709a114c6d23454454f52e
Marc Garreau <marcdgarreau@gmail.com>
2020-01-23 16:26:07 -0700

    wip: introduces react, dropzone, select

--- server.js
+++ server.js
@@ -10,43 +10,50 @@ const injector = require('./injector.js')
 
 const repository = '/repository/'
 
-app.use(express.static('public'));
-app.use(fileUpload({
+app.use(express.static('ui/dist'))
+app.use(
+  fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 },
     abortOnLimit: true
-}))
+  })
+)
 
-app.get('/', (req, res) => res.sendFile('public/index.html'))
-app.use('/repository', express.static(repository), serveIndex(repository, {'icons': true}))
-
+app.get('/', (req, res) => res.sendFile('ui/dist/index.html'))
+app.use(
+  '/repository',
+  express.static(repository),
+  serveIndex(repository, { icons: true })
+)
 
 app.post('/', (req, res) => {
-    let files = []
-    for (var x in req.files.files) {
-        if (req.files.files[x].data) {
-            files.push(req.files.files[x].data.toString())
-        } else {
-            console.log("File " + x + " invalid!")
-        }
+  let files = []
+  for (var x in req.files.files) {
+    if (req.files.files[x].data) {
+      files.push(req.files.files[x].data.toString())
+    } else {
+      console.log(`File ${x} invalid!`)
     }
-    let result = injector.inject(
-        repository,
-        req.body.chain,
-        req.body.address,
-        files
-    ).then(result => {
-        let mainAddress = result[0]
-        let path = `/repository/contract/${req.body.chain}/${mainAddress}/`
-        res.send(
-            "<html><body>Contract successfully verified!<br/>" +
-            `<a href=\"${path}\">${mainAddress}</a><br/>` +
-            (result.length > 1 ? `Found ${result.length} other addresses of this contract: ${result.join(', ')}<br/>` : "") +
-            "<a href=\"/\">Verify another one</a>" +
-            "</body></html>"
-        )
-    }).catch(err => {
-        console.log("Error!")
-        res.send("Error: " + err)
+  }
+  let result = injector
+    .inject(repository, req.body.chain, req.body.address, files)
+    .then(result => {
+      let mainAddress = result[0]
+      let path = `/repository/contract/${req.body.chain}/${mainAddress}/`
+      res.status(200).send({ result, path })
+
+      // '<html><body>Contract successfully verified!<br/>' +
+      // `<a href=\"${path}\">${mainAddress}</a><br/>` +
+      // (result.length > 1
+      // ? `Found ${
+      // result.length
+      // } other addresses of this contract: ${result.join(', ')}<br/>`
+      // : '') +
+      // '<a href="/">Verify another one</a>' +
+      // '</body></html>'
+    })
+    .catch(err => {
+      console.log(`Error: ${err}`)
+      res.status(400).send({ error: err })
     })
 })
 
