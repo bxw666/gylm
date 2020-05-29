5ca995aab25a979c11cb565d3602aadc7212f220
Edi <edi.sinovcic@gmail.com>
2020-05-29 19:23:19 +0200

    Updating publish script

--- src/ipfs/publish_to_ipfs.sh
+++ src/ipfs/publish_to_ipfs.sh
@@ -8,4 +8,4 @@ echo $hash
 curl -X POST "https://ipfs.komputing.org/api/v0/pin/add?arg=$hash&recursive=true"
 ipfs name publish $hash
 
-/app/source_verify_ens_updater/bin/source_verify_ens_updater $IPFS_SECRET /app/repository
+/app/source_verify_ens_updater/bin/source_verify_ens_updater /app/repository
